d3be64226b3f8716ffaf7815725f660f83df3861
Jeremy Greze <jereze@users.noreply.github.com>
2025-02-13 18:26:19 +0100

    fix table names

--- webapp/app/lib/data.ts
+++ webapp/app/lib/data.ts
@@ -10,19 +10,19 @@ export async function fetchExample() {
     const connection = await db.connect();
 
     const result = await connection.runAndReadUntil(
-      "SELECT * from sise_resultats",
+      "SELECT * from edc_resultats",
       ROW_TARGET_COUNT
     );
 
     // Example of query with a group by :
     // const result = await connection.runAndReadUntil(
-    //   "SELECT qualitparam, count(*) from sise_resultats group by qualitparam",
+    //   "SELECT qualitparam, count(*) from edc_resultats group by qualitparam",
     //   ROW_TARGET_COUNT //(Rows are read in chunks of 2048.)
     // );
 
     // Example of a prepared statement :
     // const prepared = await connection.prepare(
-    //   "SELECT qualitparam, count(*) from sise_resultats where qualitparam = $1 group by qualitparam"
+    //   "SELECT qualitparam, count(*) from edc_resultats where qualitparam = $1 group by qualitparam"
     // );
     // prepared.bindVarchar(1, "O");
     // const result = await prepared.runAndReadUntil(ROW_TARGET_COUNT);

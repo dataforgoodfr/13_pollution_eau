80e117e0ea2de18525c95d86da219a2205eb0ea4
sbs <>
2025-02-23 23:12:02 +0100

    Mise à jour avec prettier

--- webapp/app/api/CommuneFilter/route.tsx
+++ webapp/app/api/CommuneFilter/route.tsx
@@ -1,28 +1,25 @@
-import { NextRequest, NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req:NextRequest)
-{
-  const q = new URL(req.url).searchParams.get('q')
-  
-  if (!q)
-  {
-    return NextResponse.json(null)
-  }
-  else
-  {
-    const IGNQuery =  'https://data.geopf.fr/geocodage/search?autocomplete=1&index=poi&limit=10&returntruegeometry=false&type=municipality&category=commune'
-    const URLIGN=new URL(IGNQuery)
-    URLIGN.searchParams.set("q",q)
-    const data = await fetch (URLIGN)
-                  .then((response)=>{
-                    const data=response.clone ()
-                    //console.log("body",response)
-                    return data.json()})
-                  .catch((err)=>
-                    {
-                      console.log("fetch error :", err)
-                    })
+export async function GET(req: NextRequest) {
+  const q = new URL(req.url).searchParams.get("q");
 
-    return NextResponse.json(data)
+  if (!q) {
+    return NextResponse.json(null);
+  } else {
+    const IGNQuery =
+      "https://data.geopf.fr/geocodage/search?autocomplete=1&index=poi&limit=10&returntruegeometry=false&type=municipality&category=commune";
+    const URLIGN = new URL(IGNQuery);
+    URLIGN.searchParams.set("q", q);
+    const data = await fetch(URLIGN)
+      .then((response) => {
+        const data = response.clone();
+        //console.log("body",response)
+        return data.json();
+      })
+      .catch((err) => {
+        console.log("fetch error :", err);
+      });
+
+    return NextResponse.json(data);
   }
 }
--- webapp/components/CommuneFilter.tsx
+++ webapp/components/CommuneFilter.tsx
@@ -1,69 +1,79 @@
-"use client"
-import { Combobox, ComboboxInput, ComboboxOption, ComboboxOptions } from "@headlessui/react";
+"use client";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxOption,
+  ComboboxOptions,
+} from "@headlessui/react";
 import { Feature } from "maplibre-gl";
 import { Dispatch, SetStateAction, useState } from "react";
 
-export default function CommuneFilter() 
-{
-  const [filterString,setFilterString]=useState('')
-  const [delayHandler, setDelayHandler]=useState<NodeJS.Timeout|null>(null )
-  const [communesList, setCommunesList]=useState([])
+export default function CommuneFilter() {
+  const [filterString, setFilterString] = useState("");
+  const [delayHandler, setDelayHandler] = useState<NodeJS.Timeout | null>(null);
+  const [communesList, setCommunesList] = useState([]);
 
-  async function  HandleFilterChange(e:React.ChangeEvent<HTMLInputElement>)
-  {
-    if (!e?.target?.value)
-    {
-      setFilterString('')
-      setCommunesList([])
-      return
+  async function HandleFilterChange(e: React.ChangeEvent<HTMLInputElement>) {
+    if (!e?.target?.value) {
+      setFilterString("");
+      setCommunesList([]);
+      return;
     }
 
-    if (delayHandler)
-    {
-      clearTimeout(delayHandler)
+    if (delayHandler) {
+      clearTimeout(delayHandler);
     }
 
     setFilterString(e.target.value);
-  
-    setDelayHandler(setTimeout(()=>{
-        PerformSearch(e.target.value, setCommunesList)
-      },200))
-    
+
+    setDelayHandler(
+      setTimeout(() => {
+        PerformSearch(e.target.value, setCommunesList);
+      }, 200)
+    );
   }
 
   function SelectCommuneChanged(value: string | null): void {
     throw new Error("Function not implemented.");
   }
 
-  return <div>
-    <Combobox value={filterString} onChange={SelectCommuneChanged} >
-      <ComboboxInput className="bg-white-500 border-blue-500"
-        aria-label="Communes"
-        displayValue={(c:Feature) => c?.properties?.toponym}
-        onChange={HandleFilterChange}
-      />
-      <ComboboxOptions  anchor="bottom" className="border empty:invisible">
-        {communesList.map((C:Feature) => (
-          <ComboboxOption key={C.properties.extrafields.cleabs} value={C} className="data-[focus]:bg-blue-100 bg-white">
-            {C?.properties.toponym}
-          </ComboboxOption>
-        ))}
-      </ComboboxOptions>
-    </Combobox>
-  </div>
+  return (
+    <div>
+      <Combobox value={filterString} onChange={SelectCommuneChanged}>
+        <ComboboxInput
+          className="bg-white-500 border-blue-500"
+          aria-label="Communes"
+          displayValue={(c: Feature) => c?.properties?.toponym}
+          onChange={HandleFilterChange}
+        />
+        <ComboboxOptions anchor="bottom" className="border empty:invisible">
+          {communesList.map((C: Feature) => (
+            <ComboboxOption
+              key={C.properties.extrafields.cleabs}
+              value={C}
+              className="data-[focus]:bg-blue-100 bg-white"
+            >
+              {C?.properties.toponym}
+            </ComboboxOption>
+          ))}
+        </ComboboxOptions>
+      </Combobox>
+    </div>
+  );
 }
 
-async function PerformSearch(FilterString:string, SetCommunesListCallback:Dispatch<SetStateAction<never[]>>) 
-{
-  const data = await fetch("api/CommuneFilter?q=" + FilterString)
-    .then((response) => {
+async function PerformSearch(
+  FilterString: string,
+  SetCommunesListCallback: Dispatch<SetStateAction<never[]>>
+) {
+  const data = await fetch("api/CommuneFilter?q=" + FilterString).then(
+    (response) => {
       return response.json();
-    });
+    }
+  );
   if (data?.features) {
     SetCommunesListCallback(data?.features);
-  }
-
-  else {
+  } else {
     SetCommunesListCallback([]);
   }
 }

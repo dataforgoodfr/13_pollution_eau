5863e36f06fd7534693602f570014691d8156483
llf <fifidemacici@gmail.com>
2025-02-24 19:25:47 +0100

    Apply formatting fixes from ruff-format

--- pipelines/utils/storage_client.py
+++ pipelines/utils/storage_client.py
@@ -10,6 +10,8 @@ from tqdm import tqdm
 """Client class to interact with Scaleway Object Storage."""
 
 logger = logging.getLogger(__name__)
+
+
 class ObjectStorageClient:
     region_name = "fr-par"
     endpoint_url = "https://s3.fr-par.scw.cloud"
@@ -38,19 +40,24 @@ class ObjectStorageClient:
 
     def list_objects(self, prefix=None):
         try:
-            response = self.client_v4.list_objects(Bucket=self.bucket_name, Prefix=prefix)
+            response = self.client_v4.list_objects(
+                Bucket=self.bucket_name, Prefix=prefix
+            )
             if "Contents" in response:
                 return response["Contents"]
             else:
                 return []
         except ClientError as e:
-            logger.error("Error list_objects in bucket %s: %s/%s", self.bucket_name, prefix, e)
-     
+            logger.error(
+                "Error list_objects in bucket %s: %s/%s", self.bucket_name, prefix, e
+            )
 
     def download_object(self, file_key, local_path):
         # Get file size
         try:
-            meta_data = self.client_v4.head_object(Bucket=self.bucket_name, Key=file_key)
+            meta_data = self.client_v4.head_object(
+                Bucket=self.bucket_name, Key=file_key
+            )
             total_length = int(meta_data.get("ContentLength", 0))
 
             # Configure the callback to update the progress bar
@@ -64,8 +71,12 @@ class ObjectStorageClient:
                     Callback=lambda bytes_transferred: pbar.update(bytes_transferred),
                 )
         except ClientError as e:
-            logger.error("Error deleting object '%s' from S3 bucket '%s': %s", file_key, self.bucket_name, e)
-
+            logger.error(
+                "Error deleting object '%s' from S3 bucket '%s': %s",
+                file_key,
+                self.bucket_name,
+                e,
+            )
 
     def upload_object(self, local_path, file_key=None, public_read=False):
         try:
@@ -79,9 +90,19 @@ class ObjectStorageClient:
                 ExtraArgs={"ACL": "public-read"} if public_read else None,
             )
         except ClientError as e:
-            logger.error("Boto Client Error upload_object '%s' to bucket '%s': %s", local_path, self.bucket_name, e)
+            logger.error(
+                "Boto Client Error upload_object '%s' to bucket '%s': %s",
+                local_path,
+                self.bucket_name,
+                e,
+            )
         except Exception as e:
-            logger.error("Error upload_object '%s' to bucket '%s': %s", local_path, self.bucket_name, e)    
+            logger.error(
+                "Error upload_object '%s' to bucket '%s': %s",
+                local_path,
+                self.bucket_name,
+                e,
+            )
 
     def upload_dataframe(self, df, file_key):
         csv_buffer = io.StringIO()
@@ -93,7 +114,9 @@ class ObjectStorageClient:
                 Bucket=self.bucket_name, Key=file_key, Body=csv_buffer.getvalue()
             )
         except ClientError as e:
-            logger.error("Error upload_dataframe to S3 bucket '%s': %s", self.bucket_name, e)
+            logger.error(
+                "Error upload_dataframe to S3 bucket '%s': %s", self.bucket_name, e
+            )
 
     def read_object_as_dataframe(self, file_key):
         try:
@@ -102,11 +125,21 @@ class ObjectStorageClient:
             df = pd.read_csv(io.StringIO(csv_data))
             return df
         except ClientError as e:
-            logger.error("Error read_object_as_dataframe '%s' from S3 bucket '%s': %s", file_key, self.bucket_name, e)
+            logger.error(
+                "Error read_object_as_dataframe '%s' from S3 bucket '%s': %s",
+                file_key,
+                self.bucket_name,
+                e,
+            )
             return pd.DataFrame()
 
     def delete_object(self, key):
         try:
             self.client_v4.delete_object(Bucket=self.bucket_name, Key=key)
         except ClientError as e:
-            logger.error("Error delete_object '%s' from S3 bucket '%s': %s", key, self.bucket_name, e)
+            logger.error(
+                "Error delete_object '%s' from S3 bucket '%s': %s",
+                key,
+                self.bucket_name,
+                e,
+            )

8d2ddddb2dc2bfd2d069e07b1c7b70a0a2526f64
sbs <>
2025-03-01 18:38:17 +0100

    Merge main changes. Resolve conflicts. Updated combo to shdcn

--- webapp/components/CommuneFilter.tsx
+++ webapp/components/CommuneFilter.tsx
@@ -1,76 +1,155 @@
-"use client"
-import { Combobox, ComboboxInput, ComboboxOption, ComboboxOptions } from "@headlessui/react";
-import { ChangeEventHandler, useEffect, useState } from "react";
+"use client";
+import {
+  Dispatch,
+  Fragment,
+  SetStateAction,
+  use,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { Button } from "./ui/button";
+import {
+  Command,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "./ui/command";
+import { Feature } from "maplibre-gl";
+import { Check, ChevronsUpDown } from "lucide-react";
+import { Input } from "./ui/input";
+import { cn } from "@/lib/utils";
+import { PopoverAnchor, PopoverPortal } from "@radix-ui/react-popover";
+import { Container } from "postcss";
+import Container_, { ContainerWithChildren } from "postcss/lib/container";
+import { start } from "node:repl";
 
-export default function CommuneFilter() 
-{
-  const [FilterString,SetFilterString]=useState('')
-  const [DelayHandler, SetDelayHandler]=useState(null)
-  const [CommunesList, SetCommunesList]=useState([])
+export interface CommuneType {
+  nom: string;
+  CP: string;
+  _geopoint: string;
+  Centroid: Centroid;
+  INSEE: string;
+}
 
-  useEffect(()=>{
-    function CallFilter()
-    {
-      const Data=fetch("/api/CommuneFilter?q="+FilterString)
-      console.log("returned ",Data)
+export interface Centroid {
+  Lon: number;
+  Lat: number;
+}
+
+export default function CommuneFilter() {
+  const [filterString, setFilterString] = useState("");
+  const [delayHandler, setDelayHandler] = useState<NodeJS.Timeout | null>(null);
+  const [communesList, setCommunesList] = useState([]);
+  const [DropDownIsOpened, setDropDownOpen] = useState(false);
+  const [selectedCommune, setSelectedCommune] = useState<Feature | null>(null);
+
+  async function PerformSearch(
+    FilterString: string,
+    SetCommunesListCallback: Dispatch<SetStateAction<never[]>>,
+  ) {
+    const data = await fetch("api/CommuneFilter?q=" + FilterString).then(
+      (response) => {
+        return response.json();
+      },
+    );
+    if (data?.features) {
+      SetCommunesListCallback(data?.features);
+      setDropDownOpen(true);
+    } else {
+      SetCommunesListCallback([]);
+      setDropDownOpen(false);
     }
-
-    if (DelayHandler)
-    {
-      clearTimeout(DelayHandler)
-    }
-
-    //SetDelayHandler(setTimeout(CallFilter),100)
-    console.log("Current FilterString",FilterString)
-  },[FilterString, DelayHandler])
-
-
-
-  async function  HandleFilterChange(e:React.ChangeEvent<HTMLInputElement>)
-  {
-    if (!e?.target?.value)
-    {
-      SetFilterString('')
-      SetCommunesList([])
-      return
-    }
-    const data=await fetch("api/CommuneFilter?q="+e.target.value)
-                        .then((response)=>
-                        {
-                          return response.json()  
-                        })
-    console.log("changing",e.target.value, data)
-    SetFilterString(e.target.value)
-    if (data?.features)
-    {
-      SetCommunesList(data?.features)
-    }
-    else
-    {
-      SetCommunesList([])
-    }
-    
   }
 
-  function SetSelected(event: ChangeEvent<HTMLInputElement>): void 
-  {
-    
+  async function HandleFilterChange(e: React.ChangeEvent<HTMLInputElement>) {
+    console.log("HandleFilter", e);
+    if (!e?.target?.value) {
+      setFilterString("");
+      setCommunesList([]);
+      return;
+    }
+
+    if (delayHandler) {
+      clearTimeout(delayHandler);
+    }
+
+    setFilterString(e.target.value);
+
+    setDelayHandler(
+      setTimeout(() => {
+        PerformSearch(e.target.value, setCommunesList);
+      }, 200),
+    );
   }
 
-  return <div>
-    <Combobox value={FilterString} onChange={HandleFilterChange} >
-      <ComboboxInput
-        aria-label="Communes"
-        displayValue={(c) => c?.properties?.toponym}
-        onChange={HandleFilterChange}
-      />
-      <ComboboxOptions anchor="bottom" className="border empty:invisible">
-        {CommunesList.map((C) => (
-          <ComboboxOption key={C.properties.extrafields.cleabs} value={C} className="data-[focus]:bg-blue-100">
-            {C?.properties.toponym}
-          </ComboboxOption>
-        ))}
-      </ComboboxOptions>
-    </Combobox>
-  </div>
+  //console.log("Commuunes ", communesList, DropDownIsOpened);
+
+  return (
+    <Popover open={DropDownIsOpened} onOpenChange={setDropDownOpen}>
+      <PopoverAnchor>
+        <Input
+          className="float max-w-fit rounded-sm outline-1 outline-blue-500"
+          key="TextInputCommune"
+          value={filterString}
+          placeholder="Saisir le nom de votre commune"
+          onChange={HandleFilterChange}
+          autoFocus={true}
+        />
+      </PopoverAnchor>
+      <PopoverContent className="" asChild={true}>
+        <Command>
+          <CommandList>
+            <CommandGroup key="CommuneList">
+              {communesList?.map((x: Feature) => (
+                <CommandItem key={x?.properties?.extrafields.cleabs}>
+                  <HilightLabel
+                    value={
+                      x?.properties?.toponym +
+                      " (" +
+                      x?.properties?.postcode +
+                      ")"
+                    }
+                    HilightText={filterString}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
+
+function HilightLabel(props) {
+  if (!props?.value || !props?.HilightText) {
+    return <span>props?.value</span>;
+  }
+  const text: string = props.value;
+  const subString = props?.HilightText
+    ? props.HilightText?.toLowerCase().substring(0, text.length)
+    : "";
+  const startIdx = text.toLowerCase().indexOf(subString);
+  if (startIdx == -1) {
+    return <Fragment>{text}</Fragment>;
+  }
+
+  // TODO take care of -
+  const S1 = text.substring(0, startIdx);
+  const S2 = text.substring(startIdx, startIdx + subString?.length);
+  const S3 =
+    S2?.length < text.length
+      ? text.substring(startIdx + subString?.length, text.length)
+      : "";
+
+  return (
+    <Fragment>
+      <p className=" inline-block p-0">{S1}</p>
+      <p className=" inline-block bg-yellow-400 p-0">{S2}</p>
+      {S3}
+    </Fragment>
+  );
 }
--- webapp/components/Map.tsx
+++ webapp/components/Map.tsx
@@ -1,35 +1,37 @@
 "use client";
 
 import { useEffect, useMemo, useRef, useState } from "react";
-import maplibregl, { GeoJSONSourceSpecification, LngLat, Map, MapMouseEvent } from "maplibre-gl";
+import maplibregl, {
+  GeoJSONSourceSpecification,
+  LngLat,
+  Map,
+  MapMouseEvent,
+} from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
-import Communes,{CommuneType} from "./Communes";
 
+import CommuneFilter, { CommuneType } from "./CommuneFilter";
 
+const MAP_SOURCES_COMMUNES_GEOJSON = "CommuneData";
+const MAP_LAYER_COMMUNES_DOTS = "CommunesDotsLayer";
 
-
-const MAP_SOURCES_COMMUNES_GEOJSON = "CommuneData"
-const MAP_LAYER_COMMUNES_DOTS = 'CommunesDotsLayer'
-
-const GeoJsonSpecs:GeoJSONSourceSpecification={
-  type: 'geojson',
-  data: {type:'FeatureCollection',features:[]},
-  cluster: false,    
-} 
+const GeoJsonSpecs: GeoJSONSourceSpecification = {
+  type: "geojson",
+  data: { type: "FeatureCollection", features: [] },
+  cluster: false,
+};
 
 export default function MainMap() {
-
-  
   const mapContainer = useRef<HTMLDivElement>(null);
   const map = useRef<maplibregl.Map | null>(null);
-  const Dlist:CommuneType[]=[]
-  const MapDefaultCenter= useMemo(()=>{ return new LngLat(2.213749, 46.227638)},[])
-  const [CommunesBaseData,SetCommunesBaseData]=useState(null)
-  const [FilterString,SetFilterString]=useState("")
-  const [PopupObject,SetPopupObject]=useState(null)
+  const Dlist: CommuneType[] = [];
+  const MapDefaultCenter = useMemo(() => {
+    return new LngLat(2.213749, 46.227638);
+  }, []);
+  const [CommunesBaseData, SetCommunesBaseData] = useState(null);
+  const [FilterString, SetFilterString] = useState("");
+  const [PopupObject, SetPopupObject] = useState(null);
 
-  useEffect(() => 
-  {
+  useEffect(() => {
     if (map.current || !mapContainer.current) return;
 
     map.current = new maplibregl.Map({
@@ -39,98 +41,92 @@ export default function MainMap() {
       zoom: 5,
     });
 
-    map.current.on('load', ()=>{ InitCommunesLayer(map.current as Map,SetPopupObject)})
+    map.current.on("load", () => {
+      InitCommunesLayer(map.current as Map, SetPopupObject);
+    });
     //map.current.on('sourcedata', (x)=>{console.log("SourceDataEvt",x)})
     return () => {
       map.current?.remove();
     };
-  }
-  , [map, MapDefaultCenter]);
+  }, [map, MapDefaultCenter]);
 
-useEffect(()=>{
-     if (CommunesBaseData)
-      {
-        return
-      }
-    
+  useEffect(() => {
+    if (CommunesBaseData) {
+      return;
+    }
+
     const fetchData = async () => {
-     
-      const response = await fetch('/api/CommunesServer')
-      const jsonData = await response.json()
-      SetCommunesBaseData(jsonData)
-      const Features = GetCommunesFeatures(jsonData,FilterString)
+      const response = await fetch("/api/CommunesServer");
+      const jsonData = await response.json();
+      SetCommunesBaseData(jsonData);
+      const Features = GetCommunesFeatures(jsonData, FilterString);
 
-      GeoJsonSpecs.data.features=Features      
+      GeoJsonSpecs.data.features = Features;
     };
 
     fetchData();
-  },[FilterString, CommunesBaseData])
+  }, [FilterString, CommunesBaseData]);
 
-  useEffect(()=>{
-      if (!CommunesBaseData)
-      {
-        return
-      }
-      const Features = GetCommunesFeatures(CommunesBaseData,FilterString)
-      GeoJsonSpecs.data.features=Features   
-      const Src = map.current.getSource(MAP_SOURCES_COMMUNES_GEOJSON)
-      if (Src)
-      {
-        Src.setData(GeoJsonSpecs.data)
-      }
-      else
-      {
-        map.current?.addSource(MAP_SOURCES_COMMUNES_GEOJSON,GeoJsonSpecs)
-      }
-    
-  },[FilterString, CommunesBaseData])
+  useEffect(() => {
+    if (!CommunesBaseData) {
+      return;
+    }
+    const Features = GetCommunesFeatures(CommunesBaseData, FilterString);
+    GeoJsonSpecs.data.features = Features;
+    const Src = map.current.getSource(MAP_SOURCES_COMMUNES_GEOJSON);
+    if (Src) {
+      Src.setData(GeoJsonSpecs.data);
+    } else {
+      map.current?.addSource(MAP_SOURCES_COMMUNES_GEOJSON, GeoJsonSpecs);
+    }
+  }, [FilterString, CommunesBaseData]);
 
-  function UpdateDisplatedCommunes(CList:CommuneType[], FString:string)
-  {
-    SetFilterString(FString)
+  function UpdateDisplatedCommunes(CList: CommuneType[], FString: string) {
+    SetFilterString(FString);
   }
 
-  return <div>
-          <Communes CommunesData={CommunesBaseData} DisplayedCommunesListChanged={UpdateDisplatedCommunes}/>
-          <div ref={mapContainer} className="w-full h-[calc(100vh-8rem)]" />
-        </div>;
-  
-  }
+  return (
+    <div className="grid gap-2">
+      <CommuneFilter className="grid grid-cols-4 sm:grid-cols-1 p-1" />
+      <div
+        ref={mapContainer}
+        className="w-full h-[calc(100vh-8rem)] grid-cols-12"
+      />
+    </div>
+  );
+}
 
-function InitCommunesLayer(_map: maplibregl.Map, SetPopupObjectFn) 
-{
+function InitCommunesLayer(_map: maplibregl.Map, SetPopupObjectFn) {
+  console.log("Initing Map", _map);
 
-  console.log("Initing Map", _map)
-  
-  const src = _map.getSource(MAP_SOURCES_COMMUNES_GEOJSON)
+  const src = _map.getSource(MAP_SOURCES_COMMUNES_GEOJSON);
 
-  if (!src)
-  {
-    _map.addSource(MAP_SOURCES_COMMUNES_GEOJSON,GeoJsonSpecs)
+  if (!src) {
+    _map.addSource(MAP_SOURCES_COMMUNES_GEOJSON, GeoJsonSpecs);
   }
   _map.addLayer({
     id: MAP_LAYER_COMMUNES_DOTS,
-    type: 'circle',
+    type: "circle",
     source: MAP_SOURCES_COMMUNES_GEOJSON,
     //filter: ['!', ['has', 'point_count']],
     paint: {
-        'circle-color': '#11b4da',
-        'circle-radius': ['get','radius'],
-        'circle-stroke-width': 0.5,
-        'circle-stroke-color': '#fff'
-    }
+      "circle-color": "#11b4da",
+      "circle-radius": ["get", "radius"],
+      "circle-stroke-width": 0.5,
+      "circle-stroke-color": "#fff",
+    },
   });
 
   // Create a popup, but don't add it to the map yet.
   const popup = new maplibregl.Popup({
     closeButton: false,
-    closeOnClick: false
+    closeOnClick: false,
   });
   //SetPopupObjectFn(popup)
 
-  _map.on('mouseenter', MAP_LAYER_COMMUNES_DOTS, (e:MapMouseEvent) => {
+  _map.on("mouseenter", MAP_LAYER_COMMUNES_DOTS, (e: MapMouseEvent) => {
     // Change the cursor style as a UI indicator.
-    _map.getCanvas().style.cursor = 'pointer';
+    _map.getCanvas().style.cursor = "pointer";
 
     const coordinates = e.features[0].geometry.coordinates.slice();
     const description = e.features[0].properties.nom;
@@ -139,43 +135,41 @@ function InitCommunesLayer(_map: maplibregl.Map, SetPopupObjectFn)
     // copies of the feature are visible, the popup appears
     // over the copy being pointed to.
     while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
     }
 
     // Populate the popup and set its coordinates
     // based on the feature found.
     popup.setLngLat(coordinates).setHTML(description).addTo(_map);
-});
+  });
 
-  _map.on('mouseleave', MAP_LAYER_COMMUNES_DOTS, () => {
-    _map.getCanvas().style.cursor = '';
+  _map.on("mouseleave", MAP_LAYER_COMMUNES_DOTS, () => {
+    _map.getCanvas().style.cursor = "";
     popup.remove();
   });
-  
 }
-function GetCommunesFeatures(jsonData: CommuneType[],FilterString:string) 
-{
-  return jsonData.map((x)=>{return CommuneType2GeoJSON(x,FilterString)})
+function GetCommunesFeatures(jsonData: CommuneType[], FilterString: string) {
+  return jsonData.map((x) => {
+    return CommuneType2GeoJSON(x, FilterString);
+  });
 }
 
-function CommuneType2GeoJSON(C:CommuneType, FilterString:string)
-{
-  const Coords=C._geopoint.split(",").map(parseFloat)
-  const RetValue= {
-    type:'symbol',
-    properties:{
-      id:C.INSEE,
-      nom:C.nom,
-      selected:FilterString!==""&& C.nom.includes(FilterString?.toUpperCase()),
-      radius:3
+function CommuneType2GeoJSON(C: CommuneType, FilterString: string) {
+  const Coords = C._geopoint.split(",").map(parseFloat);
+  const RetValue = {
+    type: "symbol",
+    properties: {
+      id: C.INSEE,
+      nom: C.nom,
+      selected:
+        FilterString !== "" && C.nom.includes(FilterString?.toUpperCase()),
+      radius: 3,
     },
-    geometry:{
-      type:'Point',
-      coordinates: [Coords[1],Coords[0]]
-    }
-  }
-  RetValue.properties.radius=RetValue.properties.selected?8:3
-  return RetValue
-
+    geometry: {
+      type: "Point",
+      coordinates: [Coords[1], Coords[0]],
+    },
+  };
+  RetValue.properties.radius = RetValue.properties.selected ? 8 : 3;
+  return RetValue;
 }
-

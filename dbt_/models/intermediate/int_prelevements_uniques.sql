3609b4f2ef5edb51347200a91b735a6e0beb303d
Vinca PRANA <11522296+20k-P@users.noreply.github.com>
2025-03-06 10:32:18 +0100

    fix: run pre-commit

--- dbt_/models/intermediate/int_prelevements_uniques.sql
+++ dbt_/models/intermediate/int_prelevements_uniques.sql
@@ -1,38 +1,40 @@
-WITH 
+WITH
 prelevements_cdfirstreseauamont AS (
     SELECT DISTINCT
-      referenceprel , 
-      (CASE 
-        WHEN cdreseauamont IS NULL THEN cdreseau
-        WHEN cdreseauamont IS NOT NULL THEN cdreseauamont
-      END) AS cdfirstreseauamont,
-      dateprel ,	
-      heureprel ,
-      TRY_STRPTIME(dateprel || ' ' || REPLACE(heureprel, 'h', ':'), '%Y-%m-%d %H:%M') AS datetimeprel,
-      conclusionprel ,
-      plvconformitebacterio ,	
-      plvconformitechimique ,
-      plvconformitereferencebact ,
-      plvconformitereferencechim ,
+        referenceprel,
+        dateprel,
+        heureprel,
+        conclusionprel,
+        plvconformitebacterio,
+        plvconformitechimique,
+        plvconformitereferencebact,
+        plvconformitereferencechim,
+        (CASE
+            WHEN cdreseauamont IS NULL THEN cdreseau
+            WHEN cdreseauamont IS NOT NULL THEN cdreseauamont
+        END) AS cdfirstreseauamont,
+        TRY_STRPTIME(
+            dateprel || ' ' || REPLACE(heureprel, 'h', ':'), '%Y-%m-%d %H:%M'
+        ) AS datetimeprel
     FROM
-        edc_prelevements ),
+        edc_prelevements
+),
 
 ranked AS (
     SELECT
         *,
-        ROW_NUMBER() OVER ( 
-                PARTITION BY referenceprel
-                ORDER BY
-                    dateprel,
-                    heureprel
-            )  AS row_num
-        FROM 
-            prelevements_cdfirstreseauamont
-        )
+        ROW_NUMBER() OVER (
+            PARTITION BY referenceprel
+            ORDER BY
+                dateprel,
+                heureprel
+        ) AS row_num
+    FROM
+        prelevements_cdfirstreseauamont
+)
 
-SELECT 
-    * EXCLUDE (row_num)
-FROM 
-        ranked
-WHERE 
-       row_num = 1
+SELECT * EXCLUDE (row_num)
+FROM
+    ranked
+WHERE
+    row_num = 1

e4ae8eaa69aafec9c2cf147be85ce2941cf0a32b
Vinca PRANA <11522296+20k-P@users.noreply.github.com>
2025-03-12 18:34:01 +0100

    run precommit

--- dbt_/models/intermediate/int_edc__commune_udi.sql
+++ dbt_/models/intermediate/int_edc__commune_udi.sql
@@ -7,13 +7,17 @@ udi AS (
         -- Prenons toujours le même nom de commune pour une inseecommune donnée
         MIN(nomcommune) AS nomcommune,
         -- Agréger les différentes valeurs de quartier en une liste sans doublons
-        STRING_AGG(DISTINCT quartier, ', ') FILTER (WHERE quartier IS NOT NULL AND quartier != '') AS quartiers,
+        STRING_AGG(DISTINCT quartier, ', ') FILTER (
+            WHERE quartier IS NOT NULL AND quartier != ''
+        ) AS quartiers,
         -- Agréger les différentes valeurs de nomreseau en une liste sans doublons
-        STRING_AGG(DISTINCT nomreseau, ', ') FILTER (WHERE nomreseau IS NOT NULL AND nomreseau != '') AS nomreseaux,
+        STRING_AGG(DISTINCT nomreseau, ', ') FILTER (
+            WHERE nomreseau IS NOT NULL AND nomreseau != ''
+        ) AS nomreseaux,
         -- Prendre la première date de début d'alimentation
         MIN(debutalim) AS debutalim
-    FROM 
-       {{ ref('stg_edc__communes') }}
+    FROM
+        {{ ref('stg_edc__communes') }}
     GROUP BY
         inseecommune,
         cdreseau,
@@ -22,19 +26,20 @@ udi AS (
 
 cog AS (
     SELECT
-      DEP AS code_departement,
-      REG AS code_region,
-      COM AS commune_code_insee,
-    FROM 
-      {{ ref('stg_communes__cog') }}
-)
-    SELECT
-      udi.*,
-      cog.code_departement,
-      cog.code_region
+        dep AS code_departement,
+        reg AS code_region,
+        com AS commune_code_insee
     FROM
-      udi
-    LEFT JOIN 
-      cog
-    ON 
-      udi.commune_code_insee = cog.commune_code_insee
+        {{ ref('stg_communes__cog') }}
+)
+
+SELECT
+    udi.*,
+    cog.code_departement,
+    cog.code_region
+FROM
+    udi
+LEFT JOIN
+    cog
+    ON
+        udi.commune_code_insee = cog.commune_code_insee
--- dbt_/models/intermediate/int_mesures_cat_communes_year.sql
+++ dbt_/models/intermediate/int_mesures_cat_communes_year.sql
@@ -3,34 +3,42 @@ mesures_cat_communes AS (
     SELECT
         mesures_cat.*,
         prelevement.dateprel,
-        udi.commune_code_insee AS commune_code_insee,
-    FROM 
-       {{ ref('int__mesures_cat') }} AS mesures_cat
+        udi.commune_code_insee
+    FROM
+        {{ ref('int__mesures_cat') }} AS mesures_cat
     LEFT JOIN
-       {{ ref('stg_edc__prelevement') }} AS prelevement
-    ON
-       mesures_cat.referenceprel = prelevement.referenceprel
+        {{ ref('stg_edc__prelevement') }} AS prelevement
+        ON
+            mesures_cat.referenceprel = prelevement.referenceprel
     LEFT JOIN
-       {{ ref('stg_edc__communes') }} AS udi
-    ON
-    	udi.cdreseau = prelevement.cdreseau
-        AND udi.year = extract( YEAR FROM prelevement.dateprel)    
+        {{ ref('stg_edc__communes') }} AS udi
+        ON
+            prelevement.cdreseau = udi.cdreseau
+            AND udi.year = extract(YEAR FROM prelevement.dateprel)
 )
 
-SELECT 
- extract( YEAR FROM mesures_cat_communes.dateprel) as annee,
- categorie,
- commune_code_insee,
- SUM(1) AS nb_analyses,
- SUM(case
-        when limitequal_float is not NULL and valtraduite >= limitequal_float then 1
-        else 0
-    end ) AS nb_analyses_not_ok,
- SUM(case
-        when limitequal_float is not NULL and valtraduite < limitequal_float then 1
-        else 0
-    end ) AS nb_analyses_ok,    
+SELECT
+    extract(YEAR FROM mesures_cat_communes.dateprel) AS annee,
+    mesures_cat_communes.categorie,
+    mesures_cat_communes.commune_code_insee,
+    sum(1) AS nb_analyses,
+    sum(CASE
+        WHEN
+            mesures_cat_communes.limitequal_float IS NOT NULL
+            AND mesures_cat_communes.valtraduite
+            >= mesures_cat_communes.limitequal_float
+            THEN 1
+        ELSE 0
+    END) AS nb_analyses_not_ok,
+    sum(CASE
+        WHEN
+            mesures_cat_communes.limitequal_float IS NOT NULL
+            AND mesures_cat_communes.valtraduite
+            < mesures_cat_communes.limitequal_float
+            THEN 1
+        ELSE 0
+    END) AS nb_analyses_ok
 FROM
- mesures_cat_communes
-GROUP BY 
-    1,2,3
+    mesures_cat_communes
+GROUP BY
+    1, 2, 3

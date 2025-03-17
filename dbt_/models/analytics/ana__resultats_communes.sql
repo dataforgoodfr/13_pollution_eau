bb9734bb984ea93dc816186f281f20ebe69f06f3
Vinca PRANA <11522296+20k-P@users.noreply.github.com>
2025-03-17 14:02:23 +0100

    feat: update ana__resultats_communes.sql

--- dbt_/models/analytics/ana__resultats_communes.sql
+++ dbt_/models/analytics/ana__resultats_communes.sql
@@ -1,71 +1,135 @@
-with edc_resultats as (select * from {{ ref("stg_edc__resultats") }}),
-
-edc_prelevements as (select * from {{ ref("stg_edc__prevelevements") }}),
-
-edc_communes as (select * from {{ ref("stg_edc__communes") }}),
-
-cog_communes as (select * from {{ ref("stg_communes__cog") }}),
-
-
-resultats_cvm as (
-    select
-        *,
-        (case when valtraduite > 0.5 then 1 else 0 end) as is_non_conforme
-    from edc_resultats
-    where cdparametresiseeaux = 'CLVYL'
+WITH
+annees AS (
+    SELECT unnest(generate_series(2020, 2024)) AS annee
 ),
 
-prelevements_cvm as (
-    select
-        edc_prelevements.cdreseau,
-        resultats_cvm.de_partition,
-        SUM(resultats_cvm.is_non_conforme) as nbr_resultats_non_conformes,
-        COUNT(*) as nbr_resultats_total
-    from resultats_cvm
-    left join edc_prelevements
-        on
-            resultats_cvm.referenceprel = edc_prelevements.referenceprel
-            and
-            resultats_cvm.de_partition = edc_prelevements.de_partition
-    group by edc_prelevements.cdreseau, resultats_cvm.de_partition
+cat AS (
+    SELECT DISTINCT categorie
+    FROM
+        {{ ref('int__mapping_category_simple') }}
 ),
 
-communes_cvm as (
-    select
-        edc_communes.inseecommune,
-        edc_communes.de_partition,
-        COALESCE(SUM(prelevements_cvm.nbr_resultats_non_conformes), 0)
-            as nbr_resultats_non_conformes,
-        COALESCE(SUM(prelevements_cvm.nbr_resultats_total), 0)
-            as nbr_resultats_total,
-        case
-            when
-                SUM(prelevements_cvm.nbr_resultats_non_conformes) > 0
-                then 'non conforme'
-            when SUM(prelevements_cvm.nbr_resultats_total) > 0 then 'conforme'
-            else 'non analysé'
-        end as resultat
-    from edc_communes
-    left join prelevements_cvm
-        on
-            edc_communes.cdreseau = prelevements_cvm.cdreseau
-            and
-            edc_communes.de_partition = prelevements_cvm.de_partition
-    group by edc_communes.inseecommune, edc_communes.de_partition
+year_cat AS (
+    SELECT
+        annees.annee,
+        cat.categorie
+    FROM
+        annees
+    CROSS JOIN
+        cat
 ),
 
-annees as (select UNNEST(GENERATE_SERIES(2020, 2024)) as annee)
+list_communes_uid AS (
+    SELECT DISTINCT
+        year_cat.annee,
+        year_cat.categorie,
+        com.inseecommune
+    FROM
+        {{ ref('int__lien_commune_cdreseau') }} AS com
+    FULL OUTER JOIN
+        year_cat
+        ON
+            int__lien_commune_cdreseau.de_partition = year_cat.annee
+),
 
+communes_year AS (
+    SELECT
+        de_partition AS annee,
+        categorie,
+        inseecommune,
+        sum(1) AS nb_analyses,
+        sum(CASE
+            WHEN
+                limitequal_float IS NOT NULL AND valtraduite >= limitequal_float
+                THEN 1
+            ELSE 0
+        END) AS nb_analyses_not_ok,
+        sum(CASE
+            WHEN
+                limitequal_float IS NOT NULL AND valtraduite < limitequal_float
+                THEN 1
+            ELSE 0
+        END) AS nb_analyses_ok
+    FROM
+        {{ ref('int__resultats_udi_communes') }}
+    GROUP BY
+        annee,
+        categorie,
+        inseecommune
+),
 
-select
-    cog.com as commune_code_insee,
-    cog.libelle as commune_nom,
-    a.annee,
-    COALESCE(communes_cvm.resultat, 'non analysé') as resultat_cvm
-from cog_communes as cog
-cross join
-    annees as a
-left join communes_cvm on
-    cog.com = communes_cvm.inseecommune
-    and
-    a.annee::string = communes_cvm.de_partition
+int__resultats_communes AS (
+    SELECT
+        annee,
+        inseecommune,
+        categorie,
+        coalesce(nb_analyses, 0) AS nb_analyses,
+        coalesce(nb_analyses_not_ok, 0) AS nb_analyses_not_ok,
+        coalesce(nb_analyses_ok, 0) AS nb_analyses_ok,
+        CASE
+            WHEN coalesce(nb_analyses, 0) = 0 THEN 'Pas recherché'
+            WHEN
+                coalesce(nb_analyses, 0) > 0 AND coalesce(nb_analyses_ok, 0) = 0
+                THEN 'jamais quantifié'
+            WHEN
+                categorie = 'cvm'
+                AND coalesce(nb_analyses, 0) > 0
+                AND coalesce(nb_analyses_not_ok, 0) > 1
+                THEN '> 0,5 µg/L'
+            WHEN
+                categorie = 'cvm'
+                AND coalesce(nb_analyses, 0) > 0
+                AND coalesce(nb_analyses_ok, 0) > 0
+                THEN '<= 0,5 µg/L'
+            ELSE 'Not CVM - other'
+        END AS resultat
+    FROM
+        communes_year
+),
+
+int__resultats_all_communes AS (
+    SELECT
+        list_communes_uid.annee,
+        list_communes_uid.inseecommune,
+        list_communes_uid.categorie,
+        coalesce(int__resultats_communes.nb_analyses, 0) AS nb_analyses,
+        coalesce(int__resultats_communes.nb_analyses_not_ok, 0)
+            AS nb_analyses_not_ok,
+        coalesce(int__resultats_communes.nb_analyses_ok, 0) AS nb_analyses_ok,
+        coalesce(int__resultats_communes.resultat, 'Pas recherché') AS resultat
+    FROM
+        list_communes_uid
+    LEFT JOIN
+        int__resultats_communes
+        ON
+            list_communes_uid.annee = int__resultats_communes.annee
+            AND list_communes_uid.categorie = int__resultats_communes.categorie
+            AND list_communes_uid.inseecommune
+            = int__resultats_communes.inseecommune
+),
+
+cog AS (
+    SELECT
+        dep AS code_departement,
+        reg AS code_region,
+        com AS inseecommune,
+        libelle AS commune_nom
+    FROM
+        {{ ref('stg_communes__cog') }}
+    WHERE
+        typecom = 'COM'
+)
+
+SELECT
+    int__resultats_all_communes.inseecommune,
+    cog.commune_nom,
+    'bilan annuel' AS periode,
+    int__resultats_all_communes.categorie,
+    int__resultats_all_communes.resultat,
+    date(int__resultats_all_communes.annee) AS date_prvl
+FROM
+    int__resultats_all_communes
+LEFT JOIN
+    cog
+    ON
+        int__resultats_all_communes.inseecommune = cog.inseecommune

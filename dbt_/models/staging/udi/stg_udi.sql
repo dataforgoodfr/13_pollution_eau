a8f57cfe4eea858f2e2f4380da1a9e3b28c6e3f2
llf <fifidemacici@gmail.com>
2025-03-24 19:54:38 +0100

    run pre-commit

--- dbt_/models/staging/udi/stg_udi.sql
+++ dbt_/models/staging/udi/stg_udi.sql
@@ -1,7 +1,7 @@
-SELECT 
+SELECT
     code_udi::VARCHAR AS code_udi,
     code_ins::VARCHAR AS code_ins,
     ins_nom::VARCHAR AS ins_nom,
     geom::GEOMETRY AS geom
 FROM {{ source('udi', 'atlasante_udi') }}
-WHERE code_udi IS NOT NULL 
+WHERE code_udi IS NOT NULL

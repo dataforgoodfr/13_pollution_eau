037de76f5ebaf3022e1c8afb1b2f2ae741816a8b
llf <fifidemacici@gmail.com>
2025-03-28 14:35:10 +0100

    update duck_class

--- webapp/app/lib/duckdb.ts
+++ webapp/app/lib/duckdb.ts
@@ -1,4 +1,4 @@
-import { DuckDBInstance } from "@duckdb/node-api";
+import duckdb from '@duckdb/node-api';
 import fs from "fs";
 import path from "path";
 
@@ -17,10 +17,57 @@ if (!fs.existsSync(dbFilePath)) {
 }
 
 // Create DB instance
-const db = await DuckDBInstance.create(dbFilePath, {
-  access_mode: "READ_ONLY",
-  max_memory: "512MB",
-  threads: "4",
-});
+// const db = await DuckDBInstance.create(dbFilePath, {
+//   access_mode: "READ_ONLY",
+//   max_memory: "512MB",
+//   threads: "4",
+// });
 
+class Database{
+  private static instance: Database
+  private db: duckdb.DuckDBInstance
+  private connection: duckdb.DuckDBConnection
+
+  constructor(duckdb:duckdb.DuckDBInstance, connection:duckdb.DuckDBConnection ){
+    this.db = duckdb
+    this.connection = connection
+  }
+  // use singlon pattern to ensure the unique instance
+  public static async getInstance():Promise<Database>{
+    const db = await duckdb.DuckDBInstance.create(dbFilePath, {
+        access_mode: "READ_ONLY",
+        max_memory: "512MB",
+        threads: "4",
+    });
+    const connection = await db.connect();
+    return new Database(db, connection)
+  }
+
+  async run(sql:string){
+    try{
+      const results = await this.connection.run(sql)
+      return results
+    }catch(error){
+      console.error('run error:', error);
+      throw error;
+    }
+  }
+
+  async runAndReadUntil(sql:string, rows:number){
+    try{
+      const reader = await this.connection.runAndReadUntil(sql,rows)
+      const rows = reader.getRows();
+      return rows
+    }catch(error){
+      console.error('runAndReadUntil error:', error);
+      throw error;
+    }
+  }
+
+  // const result = await connection.runAndReadUntil(
+  //   "SELECT * from ana__resultats_communes limit 10",
+  //   ROW_TARGET_COUNT,
+  // );
+
+}
 export default db;

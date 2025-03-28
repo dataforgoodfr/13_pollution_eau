f7e542d8e26f2ccda051471c4c6be25e53d4c087
Jeremy Greze <jereze@users.noreply.github.com>
2025-03-28 17:04:18 +0100

    suggestion pour les labels/couleurs des résultats

---
+++ webapp/lib/resultats.ts
@@ -0,0 +1,25 @@
+export const resultatsDetails = {
+  aucun_parametre_quantifie: {
+    label: "Aucun paramètre quantifié",
+    color: "#75D3B4",
+  },
+  somme_20pfas_inf_0_1_et_4pfas_inf_0_02: {
+    label:
+      "Au moins un paramètre quantifié mais concentration inférieure aux valeurs seuils",
+    color: "#B4E681",
+  },
+  somme_20pfas_inf_0_1_et_4pfas_sup_0_02: {
+    label:
+      "Concerntration des PFOA, PFOS, PFNA, PFHxS supérieure à la recommendation (0.02 µg/L)",
+    color: "#EFE765",
+  },
+  somme_20pfas_sup_0_1: {
+    label:
+      "Concerntration totale en PFAS supérieure à la recommendation (>0,1 µg/L)",
+    color: "#FBBD6C",
+  },
+  un_pfas_sup_valeur_sanitaire: {
+    label: "Au moins un paramètre au dessus de la limite sanitaire",
+    color: "#FB726C",
+  },
+};

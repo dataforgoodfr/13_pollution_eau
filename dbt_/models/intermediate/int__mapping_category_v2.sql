3e4a5adbcd882c5de3eeb876300b6edebb5d1058
llf <fifidemacici@gmail.com>
2025-03-29 22:30:54 +0100

    run pre-commit

--- dbt_/models/intermediate/int__mapping_category_v2.sql
+++ dbt_/models/intermediate/int__mapping_category_v2.sql
@@ -1,7 +1,7 @@
 SELECT
     categorie,
     STRING_AGG(DISTINCT souscat, ',') AS souscats,
-    STRING_AGG(DISTINCT libminparametre, ',') AS libminparametres,
+    STRING_AGG(DISTINCT libminparametre, ',') AS libminparametres
 FROM
     {{ ref('mapping_categories_v2') }}
 GROUP BY

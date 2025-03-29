485402ae4b4aa30b5efc15699aebd273b62fcf11
llf <fifidemacici@gmail.com>
2025-03-29 23:25:56 +0100

    add category dans staging

--- dbt_/models/intermediate/int__mapping_category_v2.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    categorie,
-    STRING_AGG(DISTINCT souscat, ',') AS souscats,
-    STRING_AGG(DISTINCT libminparametre, ',') AS libminparametres
-FROM
-    {{ ref('mapping_categories_v2') }}
-GROUP BY
-    categorie

e3ec43e9247782bcbcc7fa61950bf4d5a145455a
llf <fifidemacici@gmail.com>
2025-03-29 21:48:26 +0100

    update

--- dbt_/models/intermediate/int__mapping_category_simple.sql
+++ dbt_/models/intermediate/int__mapping_category_simple.sql
@@ -3,6 +3,6 @@ SELECT
     STRING_AGG(DISTINCT categorie) AS categorie,
     COUNT(DISTINCT categorie) AS nb_categorie
 FROM
-    {{ ref('mapping_categories_v2') }}
+    {{ ref('mapping_categories') }}
 GROUP BY
     cdparametresiseeaux

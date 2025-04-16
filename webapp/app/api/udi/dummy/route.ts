1849ee4df7f715421fa147d034c03e34e3115301
Thibaut Duguet <thibaut@deepomatic.com>
2025-04-16 13:53:25 +0200

    run linter

--- webapp/app/api/udi/dummy/route.ts
+++ webapp/app/api/udi/dummy/route.ts
@@ -2,5 +2,5 @@ import { NextResponse } from "next/server";
 import { mockData } from "@/app/lib/mock-data";
 
 export async function GET() {
-    return NextResponse.json(mockData["UDI12345"], { status: 200 });
+  return NextResponse.json(mockData["UDI12345"], { status: 200 });
 }

c74e38a304a9403e3b40d2599fd5d3261c1cdce6
Jeremy Greze <jereze@users.noreply.github.com>
2025-04-17 20:28:45 +0200

    update int__resultats_metabolite_specifique_udi_dernier

--- dbt_/models/intermediate/pesticide/metabolite/int__resultats_metabolite_specifique_udi_dernier.sql
+++ dbt_/models/intermediate/pesticide/metabolite/int__resultats_metabolite_specifique_udi_dernier.sql
@@ -1,206 +1,71 @@
-----------------------
--- ESA-metolachlore --
-----------------------
-
-WITH latest_esa_metolachlore_results AS (
+WITH latest_metabolite_results AS (
     SELECT
         *,
+        CASE
+            WHEN cdparametresiseeaux IN ('ESAMTC', 'MTCESA') THEN 'metabolite_esa_metolachlore'
+            WHEN
+                cdparametresiseeaux IN ('471811R', 'R471811')
+                THEN 'metabolite_chlorothalonil_r471811'
+            WHEN cdparametresiseeaux = 'CLDZ_D' THEN 'metabolite_chloridazone_desphenyl'
+            WHEN cdparametresiseeaux = 'CLDZ_MD' THEN 'metabolite_chloridazone_methyl_desphenyl'
+            WHEN cdparametresiseeaux = 'ADET' THEN 'metabolite_atrazine_desethyl'
+        END AS type_metabolite,
         ROW_NUMBER() OVER (
-            PARTITION BY cdreseau
+            PARTITION BY
+                cdreseau,
+                CASE
+                    WHEN
+                        cdparametresiseeaux IN ('ESAMTC', 'MTCESA')
+                        THEN 'metabolite_esa_metolachlore'
+                    WHEN
+                        cdparametresiseeaux IN ('471811R', 'R471811')
+                        THEN 'metabolite_chlorothalonil_r471811'
+                    WHEN cdparametresiseeaux = 'CLDZ_D'
+                        THEN 'metabolite_chloridazone_desphenyl'
+                    WHEN
+                        cdparametresiseeaux = 'CLDZ_MD'
+                        THEN 'metabolite_chloridazone_methyl_desphenyl'
+                    WHEN cdparametresiseeaux = 'ADET'
+                        THEN 'metabolite_atrazine_desethyl'
+                END
             ORDER BY datetimeprel DESC
         ) AS row_number
+
     FROM {{ ref('int__resultats_udi_communes') }}
     WHERE
         categorie = 'pesticide'
         AND
-        -- Le ESA metolachlore peut avoir deux cdparametresiseeaux
-        cdparametresiseeaux IN ('ESAMTC', 'MTCESA')
+        categorie_2 = 'metabolite'
+        AND
+        cdparametresiseeaux IN (
+            'ESAMTC', 'MTCESA', '471811R', 'R471811', 'CLDZ_D', 'CLDZ_MD', 'ADET'
+        )
         AND
         -- On garde les prélèvements de moins d'un an
         CURRENT_DATE - datetimeprel < INTERVAL 1 YEAR
-),
-
-esa_metolachlore_results AS (
-    SELECT
-        cdreseau,
-        'metabolite_esa_metolachlore' AS categorie,
-        datetimeprel AS dernier_prel_datetime,
-        'dernier_prel' AS periode,
-        CASE
-            WHEN valtraduite = 0 OR valtraduite IS null THEN 'non_quantifie'
-            WHEN valtraduite >= valeur_sanitaire_1 THEN 'sup_valeur_sanitaire'
-            WHEN valtraduite >= limite_qualite THEN 'sup_limite_qualite'
-            WHEN valtraduite >= 0.1 THEN 'inf_limite_qualite_sup_0_1'
-            WHEN valtraduite < limite_qualite THEN 'inf_limite_qualite'
-            ELSE 'erreur'
-        END AS resultat
-    FROM latest_esa_metolachlore_results
-    WHERE row_number = 1
-),
-
-----------------------------
--- Chlorothalonil R471811 --
-----------------------------
-
-latest_chlorothalonil_r471811_results AS (
-    SELECT
-        *,
-        ROW_NUMBER() OVER (
-            PARTITION BY cdreseau
-            ORDER BY datetimeprel DESC
-        ) AS row_number
-    FROM {{ ref('int__resultats_udi_communes') }}
-    WHERE
-        categorie = 'pesticide'
-        AND
-        -- Chlorothalonil R471811 peut avoir deux cdparametresiseeaux
-        cdparametresiseeaux IN ('471811R', 'R471811')
-        AND
-        -- On garde les prélèvements de moins d'un an
-        CURRENT_DATE - datetimeprel < INTERVAL 1 YEAR
-),
-
-chlorothalonil_results AS (
-    SELECT
-        cdreseau,
-        'metabolite_chlorothalonil_r471811' AS categorie,
-        datetimeprel AS dernier_prel_datetime,
-        'dernier_prel' AS periode,
-        CASE
-            WHEN valtraduite = 0 OR valtraduite IS null THEN 'non_quantifie'
-            WHEN valtraduite >= valeur_sanitaire_1 THEN 'sup_valeur_sanitaire'
-            WHEN valtraduite >= limite_qualite THEN 'sup_limite_qualite'
-            WHEN valtraduite >= 0.1 THEN 'inf_limite_qualite_sup_0_1'
-            WHEN valtraduite < limite_qualite THEN 'inf_limite_qualite'
-            ELSE 'erreur'
-        END AS resultat
-    FROM latest_chlorothalonil_r471811_results
-    WHERE row_number = 1
-),
-
-----------------------------
--- Chloridazone desphényl --
-----------------------------
-
-latest_chloridazone_d_results AS (
-    SELECT
-        *,
-        ROW_NUMBER() OVER (
-            PARTITION BY cdreseau
-            ORDER BY datetimeprel DESC
-        ) AS row_number
-    FROM {{ ref('int__resultats_udi_communes') }}
-    WHERE
-        categorie = 'pesticide'
-        AND
-        cdparametresiseeaux = 'CLDZ_D'
-        AND
-        -- On garde les prélèvements de moins d'un an
-        CURRENT_DATE - datetimeprel < INTERVAL 1 YEAR
-),
-
-chloridazone_d_results AS (
-    SELECT
-        cdreseau,
-        'metabolite_chloridazone_desphenyl' AS categorie,
-        datetimeprel AS dernier_prel_datetime,
-        'dernier_prel' AS periode,
-        CASE
-            WHEN valtraduite = 0 OR valtraduite IS null THEN 'non_quantifie'
-            WHEN valtraduite >= valeur_sanitaire_1 THEN 'sup_valeur_sanitaire'
-            WHEN valtraduite >= limite_qualite THEN 'sup_limite_qualite'
-            WHEN valtraduite < limite_qualite THEN 'inf_limite_qualite'
-            ELSE 'erreur'
-        END AS resultat
-    FROM latest_chloridazone_d_results
-    WHERE row_number = 1
-),
-
------------------------------------
--- Chloridazone methyl-desphényl --
------------------------------------
-
-latest_chloridazone_md_results AS (
-    SELECT
-        *,
-        ROW_NUMBER() OVER (
-            PARTITION BY cdreseau
-            ORDER BY datetimeprel DESC
-        ) AS row_number
-    FROM {{ ref('int__resultats_udi_communes') }}
-    WHERE
-        categorie = 'pesticide'
-        AND
-        cdparametresiseeaux = 'CLDZ_MD'
-        AND
-        -- On garde les prélèvements de moins d'un an
-        CURRENT_DATE - datetimeprel < INTERVAL 1 YEAR
-),
-
-chloridazone_md_results AS (
-    SELECT
-        cdreseau,
-        'metabolite_chloridazone_methyl_desphenyl' AS categorie,
-        datetimeprel AS dernier_prel_datetime,
-        'dernier_prel' AS periode,
-        CASE
-            WHEN valtraduite = 0 OR valtraduite IS null THEN 'non_quantifie'
-            WHEN valtraduite >= valeur_sanitaire_1 THEN 'sup_valeur_sanitaire'
-            WHEN valtraduite >= limite_qualite THEN 'sup_limite_qualite'
-            WHEN valtraduite < limite_qualite THEN 'inf_limite_qualite'
-            ELSE
-                'erreur'
-        END AS resultat
-    FROM latest_chloridazone_md_results
-    WHERE row_number = 1
-),
-
------------------------
--- Atrazine déséthyl --
------------------------
-
-latest_atrazine_results AS (
-    SELECT
-        *,
-        ROW_NUMBER() OVER (
-            PARTITION BY cdreseau
-            ORDER BY datetimeprel DESC
-        ) AS row_number
-    FROM {{ ref('int__resultats_udi_communes') }}
-    WHERE
-        categorie = 'pesticide'
-        AND
-        cdparametresiseeaux = 'ADET'
-        AND
-        -- On garde les prélèvements de moins d'un an
-        CURRENT_DATE - datetimeprel < INTERVAL 1 YEAR
-),
-
-atrazine_results AS (
-    SELECT
-        cdreseau,
-        'metabolite_atrazine_desethyl' AS categorie,
-        datetimeprel AS dernier_prel_datetime,
-        'dernier_prel' AS periode,
-        CASE
-            WHEN valtraduite = 0 OR valtraduite IS null THEN 'non_quantifie'
-            WHEN valtraduite >= valeur_sanitaire_1 THEN 'sup_valeur_sanitaire'
-            WHEN valtraduite >= limite_qualite THEN 'sup_limite_qualite'
-            WHEN valtraduite < limite_qualite THEN 'inf_limite_qualite'
-            ELSE 'erreur'
-        END AS resultat
-    FROM latest_atrazine_results
-    WHERE row_number = 1
 )
 
--- Assemblage des résultats
 
-SELECT * FROM esa_metolachlore_results
-UNION ALL
-SELECT * FROM chlorothalonil_results
-UNION ALL
-SELECT * FROM chloridazone_d_results
-UNION ALL
-SELECT * FROM chloridazone_md_results
-UNION ALL
-SELECT * FROM atrazine_results
+SELECT
+    cdreseau,
+    type_metabolite AS categorie,
+    'dernier_prel' AS periode,
+    datetimeprel AS dernier_prel_datetime,
+    valtraduite AS dernier_prel_valeur,
+    1 AS nb_parametres,
+    CASE
+        WHEN valtraduite = 0 OR valtraduite IS null THEN 'non_quantifie'
+        WHEN valtraduite >= valeur_sanitaire_1 THEN 'sup_valeur_sanitaire'
+        WHEN valtraduite >= limite_qualite THEN 'sup_limite_qualite'
+        -- On applique le seuil de 0.1 uniquement pour des métabolites spécifiques
+        WHEN
+            valtraduite >= 0.1
+            AND type_metabolite IN (
+                'metabolite_esa_metolachlore', 'metabolite_chlorothalonil_r471811'
+            )
+            THEN 'inf_limite_qualite_sup_0_1'
+        WHEN valtraduite < limite_qualite THEN 'inf_limite_qualite'
+        ELSE 'erreur'
+    END AS resultat
+FROM latest_metabolite_results
+WHERE row_number = 1
---
+++ dbt_/tests/test_metabolite_specifique_results.sql
@@ -0,0 +1,76 @@
+-- dernier udi
+SELECT
+    'dernier_prel' AS periode,
+    cdreseau,
+    categorie,
+    resultat,
+    0 AS ratio_limite_qualite,
+    0 AS nb_sup_valeur_sanitaire
+FROM
+    {{ ref('int__resultats_metabolite_specifique_udi_dernier') }}
+WHERE
+    (
+        cdreseau = '001000277'
+        AND categorie = 'metabolite_chloridazone_desphenyl'
+        AND dernier_prel_datetime = TIMESTAMP '2025-01-22 11:24:00'
+        AND resultat != 'non_quantifie'
+    )
+    OR
+    (
+        cdreseau = '001000277'
+        AND categorie = 'metabolite_esa_metolachlore'
+        AND dernier_prel_datetime = TIMESTAMP '2025-01-22 11:24:00'
+        AND resultat != 'non_quantifie'
+    )
+    OR
+    (
+        cdreseau = '001000277'
+        AND categorie = 'metabolite_atrazine_desethyl'
+        AND dernier_prel_datetime = TIMESTAMP '2025-01-22 11:24:00'
+        AND resultat != 'inf_limite_qualite'
+    )
+    OR
+    (
+        cdreseau = '001000277'
+        AND categorie = 'metabolite_chloridazone_methyl_desphenyl'
+        AND dernier_prel_datetime = TIMESTAMP '2025-01-22 11:24:00'
+        AND resultat != 'non_quantifie'
+    )
+    OR
+    (
+        cdreseau = '001000277'
+        AND categorie = 'metabolite_chlorothalonil_r471811'
+        AND dernier_prel_datetime = TIMESTAMP '2025-01-22 11:24:00'
+        AND resultat != 'non_quantifie'
+    )
+    OR
+    (
+        cdreseau = '001000589'
+        AND categorie = 'metabolite_chlorothalonil_r471811'
+        AND dernier_prel_datetime = TIMESTAMP '2025-02-12 11:17:00'
+        AND resultat != 'inf_limite_qualite_sup_0_1'
+    )
+    OR
+    (
+        cdreseau = '001000589'
+        AND categorie = 'metabolite_esa_metolachlore'
+        AND dernier_prel_datetime = TIMESTAMP '2025-02-12 11:17:00'
+        AND resultat != 'sup_limite_qualite'
+    )
+    OR
+    (
+        cdreseau = '002000074'
+        AND categorie = 'metabolite_chlorothalonil_r471811'
+        AND dernier_prel_datetime = TIMESTAMP '2024-11-25 09:29:00'
+        AND resultat != 'sup_valeur_sanitaire'
+    )
+-- annuel udi
+-- UNION ALL
+-- SELECT
+--     'bilan_annuel' AS periode,
+--     cdreseau,
+--     categorie,
+--     '' AS resultat,
+--     ratio_limite_qualite,
+--     nb_sup_valeur_sanitaire
+-- FROM

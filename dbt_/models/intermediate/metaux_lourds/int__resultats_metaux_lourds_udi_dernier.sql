5bb0d0ba0ae538b5a15fc35a965f6eb216567f4e
simondac <dacosta.simon@protonmail.com>
2025-04-18 10:44:01 +0200

    ajout arsenic dernier prel

--- dbt_/models/intermediate/metaux_lourds/int__resultats_metaux_lourds_udi_dernier.sql
+++ dbt_/models/intermediate/metaux_lourds/int__resultats_metaux_lourds_udi_dernier.sql
@@ -1,68 +1,83 @@
--- Ici on ne garde que le dernier prélèvement pour chaque UDI
--- dans la dernière année
-WITH pb_dernier_prel AS (
+-- Ici on ne garde que le dernier prélèvement pour chaque UDI dans la dernière année
+WITH metaux_lourds_dernier_prel AS (
     SELECT
-        *,
+        int__resultats_udi_communes.*,
         ROW_NUMBER()
             OVER (
-                PARTITION BY cdreseau
+                PARTITION BY cdreseau, cdparametresiseeaux
                 ORDER BY datetimeprel DESC
             )
             AS row_number
     FROM
         {{ ref('int__resultats_udi_communes') }}
     WHERE
-        cdparametresiseeaux = 'PB'
+        cdparametresiseeaux IN ('PB', 'AS')
         AND
         -- On garde les prélèvements de moins d'un an
-        CURRENT_DATE - datetimeprel
-        < INTERVAL 1 YEAR
-),
-
--- Ici on reprend les résultats pour chaque dernier prélèvement pour chaque UDI
-resultats_udi_pb_dernier_prel AS (
-    SELECT
-        pb_dernier_prel.referenceprel,
-        pb_dernier_prel.cdparametresiseeaux,
-        pb_dernier_prel.valtraduite,
-        pb_dernier_prel.categorie,
-        pb_dernier_prel.limite_qualite,
-        pb_dernier_prel.valeur_sanitaire_1,
-        pb_dernier_prel.cdreseau,
-        pb_dernier_prel.datetimeprel
-    FROM
-        pb_dernier_prel
-    WHERE
-        -- On ne garde que le dernier prélèvement
-        -- pour chaque UDI (cdreseau)
-        pb_dernier_prel.row_number = 1
+        CURRENT_DATE - datetimeprel < INTERVAL 1 YEAR
 )
 
--- Puis on ajoute le résultat pour chaque cas
+-- Ici on ne prend que le prélèvement le plus récent (avec row_number = 1)
+-- pour chaque type de métaux lourds
 SELECT
-    resultats_udi_pb_dernier_prel.categorie,
-    resultats_udi_pb_dernier_prel.cdreseau,
-    resultats_udi_pb_dernier_prel.datetimeprel AS dernier_prel_datetime,
+    cdreseau,
+    datetimeprel AS dernier_prel_datetime,
     'dernier_prel' AS periode,
-    resultats_udi_pb_dernier_prel.valtraduite AS dernier_prel_valeur,
+    valtraduite AS dernier_prel_valeur,
     CASE
         WHEN
-            resultats_udi_pb_dernier_prel.valtraduite IS NULL
-            OR resultats_udi_pb_dernier_prel.valtraduite = 0
+            cdparametresiseeaux = 'PB'
+            THEN 'metaux_lourds_pb'
+        WHEN
+            cdparametresiseeaux = 'AS'
+            THEN 'metaux_lourds_as'
+    END AS categorie,
+    CASE
+        WHEN
+            cdparametresiseeaux = 'PB'
+            AND (
+                valtraduite IS NULL
+                OR valtraduite = 0
+            )
             THEN 'aucun_parametre_quantifie'
         WHEN
-            resultats_udi_pb_dernier_prel.valtraduite >= 10
+            cdparametresiseeaux = 'PB'
+            AND valtraduite >= 10
             THEN 'sup_limite_qualite'
         WHEN
-            resultats_udi_pb_dernier_prel.valtraduite >= 5
-            AND resultats_udi_pb_dernier_prel.valtraduite < 10
-            THEN 'sup_5_inf_10_limite_2036'
+            cdparametresiseeaux = 'PB'
+            AND valtraduite >= 5
+            AND valtraduite < 10
+            THEN 'sup_limite_qualite_2036'
         WHEN
-            resultats_udi_pb_dernier_prel.valtraduite < 5
+            cdparametresiseeaux = 'PB'
+            AND valtraduite < 5
             THEN 'inf_5'
+        WHEN
+            cdparametresiseeaux = 'AS'
+            AND (
+                valtraduite IS NULL
+                OR valtraduite = 0
+            )
+            THEN 'aucun_parametre_quantifie'
+        WHEN
+            cdparametresiseeaux = 'AS'
+            AND valtraduite >= 13
+            THEN 'sup_limite_qualite'
+        WHEN
+            cdparametresiseeaux = 'AS'
+            AND valtraduite >= 10
+            AND valtraduite < 13
+            THEN 'as_sup_10_inf_13'
+        WHEN
+            cdparametresiseeaux = 'AS'
+            AND valtraduite < 10
+            THEN 'inf_10'
         ELSE 'erreur'
     END AS resultat
 FROM
-    resultats_udi_pb_dernier_prel
+    metaux_lourds_dernier_prel
+WHERE
+    row_number = 1
 ORDER BY
-    resultats_udi_pb_dernier_prel.cdreseau
+    cdreseau

21f3cdb6ff343504a828c071fdde23c8a271b24e
Jeremy Greze <jereze@users.noreply.github.com>
2025-04-21 17:51:57 +0200

    add a test

--- dbt_/tests/test_nitrates_results.sql
+++ dbt_/tests/test_nitrates_results.sql
@@ -2,7 +2,6 @@
 SELECT
     'dernier relevé' AS periode,
     cdreseau,
-    categorie,
     resultat,
     0 AS nb_depassements,
     0 AS nb_prelevements,
@@ -12,56 +11,54 @@ FROM
 WHERE
     (
         cdreseau = '001000003'
-        AND categorie = 'nitrate'
         AND dernier_prel_datetime = '2025-01-21 12:35:00'
         AND resultat != 'inf_limite_qualite'
     )
     OR
     (
         cdreseau = '001000260'
-        AND categorie = 'nitrate'
         AND dernier_prel_datetime = '2024-12-06 12:02:00'
         AND resultat != 'inf_limite_qualite'
     )
     OR
     (
         cdreseau = '001000268'
-        AND categorie = 'nitrate'
         AND dernier_prel_datetime = '2025-01-13 09:57:00'
         AND resultat != 'inf_limite_qualite'
     )
     OR
     (
         cdreseau = '002000060'
-        AND categorie = 'nitrate'
         AND dernier_prel_datetime = '2024-10-18 08:50:00'
         AND resultat != 'sup_limite_qualite'
     )
     OR
     (
         cdreseau = '095000346'
-        AND categorie = 'nitrate'
         AND dernier_prel_datetime = '2025-02-27 08:35:00'
         AND resultat != 'inf_limite_qualite'
     )
     OR
     (
         cdreseau = '973000028'
-        AND categorie = 'nitrate'
         AND dernier_prel_datetime = '2024-12-02 11:30:00'
         AND resultat != 'inf_limite_qualite'
     )
     OR
     (
         cdreseau = '008000681'
-        AND categorie = 'nitrate'
         AND dernier_prel_datetime = '2025-02-12 11:38:00'
         AND resultat != 'sup_limite_qualite'
     )
     OR
     (
         cdreseau = '008000546'
-        AND categorie = 'nitrate'
         AND dernier_prel_datetime = '2025-02-07 10:38:00'
         AND resultat != 'sup_limite_qualite'
+    )
+    OR
+    (
+        cdreseau = '011000196'
+        AND dernier_prel_datetime = '2024-11-27 10:57:00'
+        AND resultat != 'inf_limite_qualite'
     )

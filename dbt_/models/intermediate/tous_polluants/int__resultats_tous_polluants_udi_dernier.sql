08617518cb9c652b56caaabc0f55773f332bd7f6
Jeremy Greze <jereze@users.noreply.github.com>
2025-04-21 23:07:44 +0200

    update dat model

--- dbt_/models/intermediate/tous_polluants/int__resultats_tous_polluants_udi_dernier.sql
+++ dbt_/models/intermediate/tous_polluants/int__resultats_tous_polluants_udi_dernier.sql
@@ -1,22 +1,22 @@
 SELECT
     cdreseau,
-    periode,
+    'tous' AS categorie,
+    'dernier_prel' AS periode,
     MAX(dernier_prel_datetime) AS dernier_prel_datetime,
-
+    SUM(nb_parametres) AS nb_parametres,
     CASE
         WHEN BOOL_OR(resultat IN (
             'sup_0_5',
-            'un_pfas_sup_valeur_sanitaire',
             'sup_valeur_sanitaire',
             'sup_valeur_sanitaire_2'
-        )) THEN 'min_1_parametre_sup_limite_sanitaire'
+        )) THEN 'sup_limite_sanitaire'
 
         WHEN BOOL_OR(resultat IN (
             'inf_limite_qualite_sup_0_1',
             'somme_20pfas_sup_0_1',
             'somme_20pfas_inf_0_1_et_4pfas_sup_0_02',
             'sup_limite_qualite'
-        )) THEN 'min_1_parametre_sup_limite_qualite'
+        )) THEN 'sup_limite_qualite'
 
         WHEN BOOL_AND(resultat IN (
             'non_quantifie',
@@ -27,8 +27,8 @@ SELECT
         )) THEN 'inf_limites'
 
         ELSE 'erreur'
-    END AS resultat_all
+    END AS resultat
 
 FROM {{ ref('int__union_resultats_udi') }}
 WHERE periode = 'dernier_prel'
-GROUP BY cdreseau, periode
+GROUP BY cdreseau
--- dbt_/tests/test_tous_polluants_results.sql
+++ dbt_/tests/test_tous_polluants_results.sql
@@ -2,24 +2,24 @@
 SELECT
     'dernier_prel' AS periode,
     cdreseau,
-    resultat_all
+    resultat
 FROM
     {{ ref('int__resultats_tous_polluants_udi_dernier') }}
 WHERE
     (
         cdreseau = '001000404'
         AND dernier_prel_datetime = '2025-01-21 12:12:00'
-        AND resultat_all != 'min_1_parametre_sup_limite_qualite'
+        AND resultat != 'sup_limite_qualite'
     )
     OR
     (
         cdreseau = '976003881'
         AND dernier_prel_datetime = '2024-06-18 10:21:00'
-        AND resultat_all != 'inf_limites'
+        AND resultat != 'inf_limites'
     )
     OR
     (
         cdreseau = '088002246'
         AND dernier_prel_datetime = '2024-10-22 10:33:00'
-        AND resultat_all != 'min_1_parametre_sup_limite_sanitaire'
+        AND resultat != 'sup_limite_sanitaire'
     )

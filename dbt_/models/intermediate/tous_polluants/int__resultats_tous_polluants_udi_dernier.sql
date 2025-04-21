0c34cd1e1c9a2a147c688088222c23b4c6f65cc3
Jeremy Greze <jereze@users.noreply.github.com>
2025-04-21 22:40:02 +0200

    Revert "fix: little changes to match the results better"
    This reverts commit 373d54ba2249cb130271ceaf103b07ceade55b0e.

--- dbt_/models/intermediate/tous_polluants/int__resultats_tous_polluants_udi_dernier.sql
+++ dbt_/models/intermediate/tous_polluants/int__resultats_tous_polluants_udi_dernier.sql
@@ -6,20 +6,22 @@ SELECT
     CASE
         WHEN BOOL_OR(resultat IN (
             'sup_0_5',
+            'un_pfas_sup_valeur_sanitaire',
             'sup_valeur_sanitaire',
             'sup_valeur_sanitaire_2'
         )) THEN 'min_1_parametre_sup_limite_sanitaire'
 
         WHEN BOOL_OR(resultat IN (
-            'inf_limite_qualite_sup_0_02',
-            'inf_limite_qualite_sup_0_1',
+            'somme_20pfas_sup_0_1',
+            'somme_20pfas_inf_0_1_et_4pfas_sup_0_02',
             'sup_limite_qualite'
         )) THEN 'min_1_parametre_sup_limite_qualite'
 
         WHEN BOOL_AND(resultat IN (
             'non_quantifie',
+            'aucun_parametre_quantifie',
             'inf_valeur_sanitaire',
-            'inf_limite_qualite',
+            'somme_20pfas_inf_0_1_et_4pfas_inf_0_02',
             'inf_0_5'
         )) THEN 'inf_limites'
 

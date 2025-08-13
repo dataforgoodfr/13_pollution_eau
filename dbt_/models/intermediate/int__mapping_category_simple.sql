020de2757a68b6eb94a8867e4a34e22692a54434
Jeremy Greze <jereze@users.noreply.github.com>
2025-08-13 13:19:38 +0200

    Remove mapping_categories seed (unused)

--- dbt_/models/intermediate/int__mapping_category_simple.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    cdparametresiseeaux,
-    STRING_AGG(DISTINCT categorie) AS categorie,
-    COUNT(DISTINCT categorie) AS nb_categorie
-FROM
-    {{ ref('mapping_categories') }}
-GROUP BY
-    cdparametresiseeaux

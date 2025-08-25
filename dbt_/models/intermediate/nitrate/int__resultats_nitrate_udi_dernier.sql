f834ebc4239eb9a5de3c464a0d31c69b8f6959d9
Jeremy Greze <jereze@users.noreply.github.com>
2025-08-25 19:00:40 +0200

    calculations for nitrates are now much simpler (following new specs)

--- dbt_/models/intermediate/nitrate/int__resultats_nitrate_udi_dernier.sql
+++ dbt_/models/intermediate/nitrate/int__resultats_nitrate_udi_dernier.sql
@@ -2,11 +2,14 @@ WITH
 last_pvl AS (
     SELECT
         cdreseau,
-        referenceprel,
+        categorie,
+        cdparametresiseeaux,
+        valeur_sanitaire_1,
         datetimeprel,
+        valtraduite,
         ROW_NUMBER()
             OVER (
-                PARTITION BY cdreseau
+                PARTITION BY cdreseau, cdparametresiseeaux
                 ORDER BY datetimeprel DESC
             )
             AS row_number
@@ -14,163 +17,43 @@ last_pvl AS (
         {{ ref('int__resultats_udi_communes') }}
     WHERE
         categorie = 'nitrate'
-        AND cdparametresiseeaux = 'NO3'
-        AND -- On garde les prélèvements de moins d'un an
+        AND
+        cdparametresiseeaux = 'NO3'
+        AND
+        -- On garde les prélèvements de moins d'un an
         CURRENT_DATE - datetimeprel < INTERVAL 1 YEAR
-        AND valtraduite IS NOT NULL
+        AND
         -- Cf cas cdreseau IN( '034005906') , referenceprel= 03400327764
-),
-
-valeur_ref AS (
-    SELECT
-        MAX(
-            CASE
-                WHEN
-                    cdparametresiseeaux = 'NO3'
-                    THEN limite_qualite
-            END
-        ) AS limite_qualite_no3,
-        MAX(
-            CASE
-                WHEN
-                    cdparametresiseeaux = 'NO3_NO2'
-                    THEN limite_qualite
-            END
-        ) AS limite_qualite_no3_no2,
-        MAX(
-            CASE
-                WHEN
-                    cdparametresiseeaux = 'NO2'
-                    THEN limite_qualite
-            END
-        ) AS limite_qualite_no2
-    FROM
-        {{ ref('int__valeurs_de_reference') }}
-    WHERE
-        categorie_1 = 'nitrate'
-),
-
-split_nitrate AS (
-    SELECT
-        resultats.cdreseau,
-        resultats.categorie,
-        resultats.datetimeprel AS date_dernier_prel,
-        COUNT(DISTINCT resultats.cdparametresiseeaux) AS nb_parametres,
-        MAX(
-            CASE
-                WHEN
-                    resultats.cdparametresiseeaux = 'NO3'
-                    THEN resultats.valtraduite
-            END
-        ) AS valtraduite_no3,
-        MAX(
-            CASE
-                WHEN
-                    resultats.cdparametresiseeaux = 'NO2'
-                    THEN resultats.valtraduite
-            END
-        ) AS valtraduite_no2,
-        MAX(
-            CASE
-                WHEN
-                    resultats.cdparametresiseeaux = 'NO3_NO2'
-                    THEN resultats.valtraduite
-            END
-        ) AS valtraduite_no3_no2
-    FROM
-        last_pvl
-    LEFT JOIN
-        {{ ref('int__resultats_udi_communes') }} AS resultats
-        ON
-            last_pvl.cdreseau = resultats.cdreseau
-            AND last_pvl.referenceprel = resultats.referenceprel
-    WHERE
-        last_pvl.row_number = 1
-        -- on ne veut que le dernier prélèvement
-        AND resultats.categorie = 'nitrate'
-        -- on ne veut que les résultats associé aux nitrates/nitrites
-    GROUP BY
-        resultats.cdreseau,
-        resultats.categorie,
-        resultats.datetimeprel
-),
-
-split_nitrate_with_ref AS (
-    SELECT
-        split_nitrate.cdreseau,
-        split_nitrate.categorie,
-        split_nitrate.nb_parametres,
-        split_nitrate.date_dernier_prel,
-        valeur_ref.limite_qualite_no3,
-        valeur_ref.limite_qualite_no2,
-        valeur_ref.limite_qualite_no3_no2,
-        split_nitrate.valtraduite_no3_no2,
-        split_nitrate.valtraduite_no3,
-        split_nitrate.valtraduite_no2
-    FROM
-        split_nitrate
-    CROSS JOIN
-        valeur_ref
+        valtraduite IS NOT NULL
 )
 
 SELECT
     cdreseau,
-    categorie,
     'dernier_prel' AS periode,
-    date_dernier_prel,
-    nb_parametres,
+    datetimeprel AS date_dernier_prel,
+    1 AS nb_parametres,
+    categorie,
     CASE
-        WHEN -- Si nitrates (no3) et pas nitrites (no2)
-            valtraduite_no2 IS NULL
-            AND
-            valtraduite_no3
-            < limite_qualite_no3
-            THEN 'inf_limite_qualite'
         WHEN
-            valtraduite_no2 IS NULL
-            AND
-            valtraduite_no3
-            >= limite_qualite_no3
-            THEN 'sup_limite_qualite'
-        WHEN -- Si nitrates (no3) ET nitrites (no2)
-            valtraduite_no2 IS NOT NULL
-            AND (
-                valtraduite_no3
-                >= limite_qualite_no3
-                OR valtraduite_no2
-                >= limite_qualite_no2
-                OR COALESCE(
-                    valtraduite_no3_no2,
-                    (
-                        valtraduite_no3 / 50
-                        + valtraduite_no2 / 3
-                    )
-                )
-                >= limite_qualite_no3_no2
-            )
-            THEN 'sup_limite_qualite'
+            valtraduite = 0
+            THEN 'non_quantifie'
         WHEN
-            valtraduite_no2 IS NOT NULL
-            AND valtraduite_no3
-            < limite_qualite_no3
-            AND valtraduite_no2
-            < limite_qualite_no2
-            AND COALESCE(
-                valtraduite_no3_no2,
-                (
-                    valtraduite_no3 / 50
-                    + valtraduite_no2 / 3
-                )
-            )
-            < limite_qualite_no3_no2
-            THEN 'inf_limite_qualite'
-        ELSE 'erreur'
+            valtraduite > valeur_sanitaire_1
+            THEN 'sup_valeur_sanitaire'
+        WHEN
+            valtraduite <= 25
+            THEN 'no3_inf_25'
+        WHEN
+            valtraduite > 25 AND valtraduite <= 40
+            THEN 'no3_inf_40'
+        WHEN
+            valtraduite > 40 AND valtraduite <= valeur_sanitaire_1
+            THEN 'inf_valeur_sanitaire'
+        ELSE 'error'
     END AS resultat,
-    JSON_OBJECT(
-        CASE WHEN valtraduite_no2 > 0 THEN 'NO2' END, valtraduite_no2,
-        CASE WHEN valtraduite_no3 > 0 THEN 'NO3' END, valtraduite_no3,
-        CASE WHEN valtraduite_no3_no2 > 0 THEN 'NO3_NO2' END, valtraduite_no3_no2
-    ) AS parametres_detectes
-
+    JSON_OBJECT(CASE WHEN valtraduite > 0 THEN cdparametresiseeaux END, valtraduite)
+        AS parametres_detectes
 FROM
-    split_nitrate_with_ref
+    last_pvl
+WHERE
+    row_number = 1

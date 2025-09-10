b018c545d9333db3252b7c1f92a13696b8461670
Jeremy Greze <jereze@users.noreply.github.com>
2025-09-10 18:09:49 +0200

    Remove ResizableWrapper component and redirect Home page to /embed

--- webapp/app/page.tsx
+++ webapp/app/page.tsx
@@ -1,27 +1,5 @@
-//import Image from "next/image";
-import { ResizableWrapper } from "@/components/ResizableWrapper";
-import PollutionMap from "@/components/PollutionMap";
-import { fetchPollutionStats } from "./lib/data";
-
-// Mise en cache de la page pour 24 heures
-export const revalidate = 86400;
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const stats = await fetchPollutionStats();
-
-  return (
-    <div className="flex flex-col h-screen w-screen">
-      <header className="p-4 bg-blue-700 text-white">
-        <h1 className="text-2xl font-bold">
-          Pollution de l&apos;Eau Potable en France
-        </h1>
-      </header>
-
-      <main className="relative flex-1 w-full">
-        <ResizableWrapper>
-          <PollutionMap pollutionStats={stats} />
-        </ResizableWrapper>
-      </main>
-    </div>
-  );
+  redirect("/embed");
 }

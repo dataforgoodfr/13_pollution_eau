9963c2bddef526e417ec33d81c617ecf783a9d06
Jeremy Greze <jereze@users.noreply.github.com>
2025-09-15 16:38:35 +0200

    misc

--- webapp/components/PollutionMap.tsx
+++ webapp/components/PollutionMap.tsx
@@ -76,7 +76,7 @@ export default function PollutionMap({
             isMobile={isMobile}
           />
 
-          <div className="absolute top-4 left-4 z-10 flex space-x-0 md:space-x-6 space-y-1 md:space-y-0 flex-col md:flex-row">
+          <div className="absolute top-4 left-4 z-10 flex gap-1 md:gap-6 flex-col md:flex-row">
             <PollutionMapFilters
               period={period}
               setPeriod={setPeriod}

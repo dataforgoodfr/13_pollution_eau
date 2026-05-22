7c6da60d5b0a9e853d3653bbbd2fca5d16bb017d
Jeremy Greze <jereze@users.noreply.github.com>
2026-05-22 18:16:58 +0200

    séparer les tests nitrates et sub_indus en fichiers udi/communes distincts

--- dbt_/tests/test_sub_indus_udis_results.sql
+++ dbt_/tests/test_sub_indus_udis_results.sql
@@ -160,25 +160,3 @@ WHERE
             nb_prelevements != 2
         )
     )
-UNION ALL
--- annuel commune
-SELECT
-    'annuel' AS periode,
-    inseecommune,
-    categorie,
-    '' AS resultat,
-    nb_depassements,
-    nb_prelevements,
-    ratio_limite_sanitaire
-FROM
-    {{ ref('int__resultats_sub_indus_commune_annuel') }}
-WHERE
-    (
-        inseecommune = '28269'
-        AND categorie = 'sub_indus_perchlorate'
-        AND annee = '2023'
-        AND (
-            nb_prelevements != 28
-            OR nb_depassements != 10
-        )
-    )
